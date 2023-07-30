ff385b963ada4a1a936de4acc2a497b369ee7806
Horace Pan <horace@gauntlet.network>
2023-07-29 22:57:38 -0400

    fix coingecko api

--- gauntlet/coingecko.py
+++ gauntlet/coingecko.py
@@ -1,4 +1,6 @@
+import os
 from abc import ABC, abstractmethod
+from dataclasses import dataclass
 import time
 import datetime
 import json
@@ -7,43 +9,65 @@ import pandas as pd
 import requests
 from constants import SYMBOL_MAP
 
+def get_coingecko_api_key():
+    return os.environ.get("COINGECKO_API_KEY")
 
+@dataclass
 class API(ABC):
-    calls_per_period: int
-    _last_call_time: float = 0 # time since the Epoch as given by time.time()
-
-    def calculate_wait_time(self) -> float:
-        dt = time.time() - self._last_call_time
-        period_length = 60 / self.requests_per_minute
-
-        if dt > period_length:
-            return 0
-        else:
-            return period_length - dt
+    def __init__(self):
+        self._last_call_time = 0 # time since the Epoch as given by time.time()
+        self._last_call_time = 0
+        self._calls_in_period = 0
 
     @property
     @abstractmethod
     def requests_per_minute(self, request) -> float:
         raise NotImplemented
 
+    @property
+    def period_duration(self):
+        return 60 / self.requests_per_minute
+
+    def calculate_wait_time(self) -> float:
+        dt = time.time() - self._last_call_time
+        period_length = 60 / self.requests_per_minute
+
+        if self._calls_in_period >= self.requests_per_minute:
+            return period_length
+        elif dt > period_length or self._calls_in_period < self.requests_per_minute:
+            # no need to wait since the elapsed time is long enough
+            return 0
+        # else:
+        #     return period_length - dt
+
     def make_request(self, **request_kwargs) -> requests.request:
         wt = self.calculate_wait_time()
-        print(wt)
+
         if wt > 0:
-            print("Waiting: {:.5f}s".format(wt))
-            time.sleep(self.calculate_wait_time()) # Obey the rate limit
+            time.sleep(wt) # Obey the rate limit
+            self._last_call_time = time.time()
+            self._calls_in_period = 0
 
+        self._calls_in_period += 1
+        header =  {"x-cg-pro-api-key": get_coingecko_api_key()}
+        request_kwargs = {**request_kwargs, "headers": header}
         response = requests.get(**request_kwargs)
-        self._last_call = time.time()
-
         if not response.ok:
             response.raise_for_status()
 
         return response
 
+    def backoff(self):
+        time.sleep(self.period)
 
 class CoinGecko(API):
     CHAIN_IDS = {"ethereum": 1}
+    PUBLIC_URL = "https://api.coingecko.com/api/v3"
+    PRO_URL = "https://pro-api.coingecko.com/api/v3"
+
+    @property
+    def api_url(self):
+        return CoinGecko.PRO_URL if get_coingecko_api_key() else CoinGecko.PUBLIC_URL
 
     @property
     def requests_per_minute(self):
@@ -51,23 +75,26 @@ class CoinGecko(API):
         Coingecko's advertised public api rate limit is 10-30 calls per minute.
         Source: https://www.coingecko.com/en/api/pricing#:~:text=Our%20Public%20API%20has%20a,the%20next%201%20minute%20window.
         '''
-        return 10
+        if get_coingecko_api_key():
+            return 500
+        else:
+            return 10
 
     def token_info(self, address: str, chain: str = "ethereum"):
         chain_id = 1 if chain == 'ethereum' else None
-        url = f"https://api.coingecko.com/api/v3/coins/{chain_id}/contract/{address}"
+        url = f"{self.api_url}/coins/{chain_id}/contract/{address}"
         response = self.make_request(url=url)
         return response.json()
 
     def current_price(self, address: str, chain: str = "ethereum", currency='usd'):
-        url = f"https://api.coingecko.com/api/v3/simple/token_price/{chain}?contract_addresses={address}&vs_currencies={currency}"
+        url = f"{self.api_url}/simple/token_price/{chain}?contract_addresses={address}&vs_currencies={currency}"
         response = self.make_request(url=url)
         resp_js = response.json()
         return resp_js[address][currency]
 
     def market_chart(self, address: str, chain: str = "ethereum", interval: str = "daily", currency: str = "usd"):
         chain_id = CoinGecko.CHAIN_IDS[chain]
-        url = f"https://api.coingecko.com/api/v3/coins/{chain_id}/contract/{address}/market_chart"
+        url = f"{self.api_url}/coins/{chain_id}/contract/{address}/market_chart"
         params = {
             "vs_currency": currency,
             "days": "max",
@@ -89,7 +116,7 @@ class CoinGecko(API):
 
 
     def ohlc(self, cg_token_id: str, currency: str = "usd"):
-        url = f"https://api.coingecko.com/api/v3/coins/{cg_token_id}/ohlc?vs_currency=usd&days=max"
+        url = f"{self.api_url}/coins/{cg_token_id}/ohlc?vs_currency=usd&days=max"
         response = self.make_request(url=url)
 
         if not response.ok:
@@ -116,11 +143,11 @@ if __name__ == "__main__":
     wsteth = "0x7f39c581f595b53c5cb19bd0b3f8da6c935e2ca0"
 
     api = CoinGecko()
-
     for i in range(10):
+        st = time.time()
         info = api.token_info(usdc)
-        print(i)
+        print("i = {} | Time for token info call: {:.2f}s".format(i, time.time() - st))
+
     price = api.current_price(usdc)
     market_chart = api.market_chart(usdc)
     ohlc = api.ohlc("usd-coin")
-    breakpoint()
