494b7fca0f35e62f94a81996037cdc14e27352ef
Horace Pan <horace@gauntlet.network>
2023-08-08 00:42:44 -0400

    doc string for cowswap

--- gauntlet/cowswap.py
+++ gauntlet/cowswap.py
@@ -1,41 +1,84 @@
-import time
 import json
+import time
+
 import requests
 from coingecko import CoinGecko
 
-def get_cowswap(token_in: str, token_out: str, token_in_decimals: int, amount: float, quality: str = "optimal"):
+
+def get_cowswap(
+    token_in: str,
+    token_out: str,
+    token_in_decimals: int,
+    amount: float,
+    quality: str = "optimal",
+) -> requests.Request:
+    """
+    Returns the raw cowswap API response for a token swap from {token_in} to {token_out}
+    of size {amount}.
+
+    token_in: str, token address of the sell token
+    token_out: str, token address of the buy token
+    token_in_decimals: int, number of decimals of token_in
+    amount: float, number of tokens of token_in to be sold
+    quality: str (optimal or fast)
+
+    """
     url = "https://api.cow.fi/mainnet/api/v1/quote"
     params = {
         "sellToken": token_in,
         "buyToken": token_out,
         "receiver": "0x0000000000000000000000000000000000000000",
         "partiallyFillable": False,
-        "sellAmountBeforeFee": str(
-            int(amount * 10 ** token_in_decimals)
-        ),
+        "sellAmountBeforeFee": str(int(amount * 10**token_in_decimals)),
         "sellTokenBalance": "erc20",
         "buyTokenBalance": "erc20",
         "kind": "sell",
         "from": "0x0000000000000000000000000000000000000000",
         "priceQuality": quality,
         "onchainOrder": False,
-        "validTo": int(time.time() + 60 * 60)
+        "validTo": int(time.time() + 60 * 60),
     }
     response = requests.post(url, json=params)
     return response.json()
 
-def get_impact(token_in: str, token_out: str, token_in_decimals: int, token_out_decimals: int, amount: float, quality: str = "optimal"):
+
+def get_impact(
+    token_in: str,
+    token_out: str,
+    token_in_decimals: int,
+    token_out_decimals: int,
+    amount: float,
+    quality: str = "optimal",
+) -> float:
+    """
+    Returns the price impact of a token swap of size {amount} from {token_in} to
+    {token_out}.
+    The price impact of a swap is computed as follows:
+    price impact = 1 - (amount received in USD / amount paid in USD)
+
+    token_in: str, token address of the sell token
+    token_out: str, token address of the buy token
+    token_in_decimals: int, number of decimals of token_in
+    token_out_decimals: int, number of decimals of token_out
+    amount: float, number of tokens of token_in to be sold
+    quality: str (optimal or fast)
+    """
     cg = CoinGecko()
     spot_in = cg.current_price(token_in)
     spot_out = cg.current_price(token_out)
 
     response = get_cowswap(token_in, token_out, token_in_decimals, amount, quality)
-    amount_in_usd = float(response['quote']['sellAmount']) / (10 ** token_in_decimals) * spot_in
-    amount_out_usd = float(response['quote']['buyAmount']) / (10 ** token_out_decimals) * spot_out
+    amount_in_usd = (
+        float(response["quote"]["sellAmount"]) / (10**token_in_decimals) * spot_in
+    )
+    amount_out_usd = (
+        float(response["quote"]["buyAmount"]) / (10**token_out_decimals) * spot_out
+    )
     price_impact = 1 - float(amount_out_usd / amount_in_usd)
     return price_impact
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     weth = "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2"
     usdc = "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48"
     amount = 100_000
@@ -46,16 +89,16 @@ if __name__ == '__main__':
     st = time.time()
     res = get_cowswap(weth, usdc, 18, amount, "fast")
     end = time.time()
-    amount_in = weth_spot * float(res['quote']['sellAmount']) / 1e18
-    amount_out = float(res['quote']['buyAmount']) / 1e6
+    amount_in = weth_spot * float(res["quote"]["sellAmount"]) / 1e18
+    amount_out = float(res["quote"]["buyAmount"]) / 1e6
     price_impact = 1 - (amount_out / amount_in)
 
-    print("Swap size of {} | Sell price: {:.2f} | Amount received: {}".format(
-        int(res['quote']['sellAmount']) / 1e18,
-        int(res['quote']['sellAmount']) / 1e18 * weth_spot,
-        int(res['quote']['buyAmount']) / 1e6,
-    ))
-    print("Price impact: {:.3f}".format(
-        price_impact
-    ))
+    print(
+        "Swap size of {} | Sell price: {:.2f} | Amount received: {}".format(
+            int(res["quote"]["sellAmount"]) / 1e18,
+            int(res["quote"]["sellAmount"]) / 1e18 * weth_spot,
+            int(res["quote"]["buyAmount"]) / 1e6,
+        )
+    )
+    print("Price impact: {:.3f}".format(price_impact))
     print("Cow swap time: {:.3f}s".format(end - st))
--- gauntlet/utils.py
+++ gauntlet/utils.py
@@ -4,8 +4,6 @@ import pprint
 import logger
 from coingecko import CoinGecko
 from cowswap import get_cowswap
-from kyberswap import get_swap_route
-from kyberswap import get_swap_route_usd
 from tokens import Tokens
 
 
@@ -19,50 +17,6 @@ def ms_to_dt(ms):
     return dt_object.strftime("%Y-%m-%d")
 
 
-def price_impact_size(
-    token_in: str,
-    token_in_decimals: int,
-    token_out: str,
-    token_out_decimals: int,
-    target_price_impact: float,
-    rtol=5e-2,
-):
-    """
-    Returns the amount of token_in required to incur a price impact of {price_impact}.
-
-    Ex: price_impact_size(weth, usdc, 0.10) returns the amount of WETH necessary to incur 10%
-    price impact (+/- 0.01).
-    """
-
-    def kyberswap_oracle(token_in: str, token_out: str, size: float, retries=3):
-        response = get_swap_route(
-            "ethereum", token_in, token_out, token_in_decimals, size
-        )
-        route = response["data"]["routeSummary"]
-        price_impact = 1 - float(route["amountOutUsd"]) / float(route["amountInUsd"])
-        return price_impact
-
-    spot = CoinGecko().current_price(token_in, "ethereum")
-    min_sz = 0
-    max_sz = 100_000_000 / spot
-    iters = 0
-    price_impact = 1  # default start to run at least one iter
-
-    while abs(1 - (price_impact / target_price_impact)) > rtol and iters < MAX_ITERS:
-        mid = (max_sz + min_sz) / 2.0
-        price_impact = kyberswap_oracle(token_in, token_out, mid)
-
-        if price_impact < target_price_impact:
-            min_sz = mid
-        else:
-            max_sz = mid
-
-        # log.info(f"{iters:2d} | x = {mid:.2f} | price impact: {price_impact:.4f}")
-        iters += 1
-
-    return (max_sz + mid) / 2.0
-
-
 # TODO: Unify interface for cow/kyber
 def price_impact_size_cowswap(
     token_in: str,
@@ -73,7 +27,12 @@ def price_impact_size_cowswap(
     rtol=5e-2,
     max_sz_usd=500_000_000,
 ):
-    def cowswap_oracle(token_in, token_out, size, retries=3):
+    def cowswap_oracle(token_in: str, token_out: str, size: float):
+        """
+        token_in: address of the sell token
+        token_out: address of the buy token
+        size: amount of token_in to sell
+        """
         response = get_cowswap(
             token_in, token_out, token_in_decimals, size, quality="fast"
         )
@@ -105,7 +64,7 @@ def price_impact_size_cowswap(
         else:
             max_sz = mid
 
-        log.info(f"{iters:2d} | x = {mid:.2f} | price impact: {price_impact:.4f}")
+        log.debug(f"{iters:2d} | x = {mid:.2f} | price impact: {price_impact:.4f}")
         iters += 1
     return (max_sz + min_sz) / 2.0
 
@@ -125,7 +84,6 @@ if __name__ == "__main__":
                 p,
                 max_sz_usd=1_000_000_000,
             )
-            log.info(tok, p, pis[tok.symbol][p])
-        log.info("-" * 20)
+            log.info(f"{tok} | impact size: {p:.3f} | swap size: {pis[tok.symbol][p]}")
 
     pprint.pp(pis)
